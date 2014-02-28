7ceabeef1e1cbdc8cb4f21af9ac5565b4489451c
eeevanbbb <eeevanbbb@me.com>
2014-02-28 15:06:04 -0800

    Added Analog Face
    Press the center button to push the analog face, then the back button to go back. The analog face is missing clock digits. It also doesn't do much in the way of translating the traditional clock face into base x. That's a design issue I'm thinking on.

--- src/main.c
+++ src/main.c
@@ -9,8 +9,10 @@ Layer *analog_layer;
 int base = 10;
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed);
+static void analog_window_config_provider(void *context);
 
 //With help from: http://www.phanderson.com/C/baseprnt.html
+//This method has an issue where it doesn't print out 0
 char * convert_to_base(int num, int base) {
 	
 	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -60,29 +62,85 @@ void analogLayer_update_callback(Layer *myLayer, GContext *ctx) {
 		
 	graphics_context_set_stroke_color(ctx, GColorWhite);
 	
-	graphics_draw_circle(ctx, GPoint(72,84), 70);
+	GPoint center = GPoint(72,84);
+	float radius = 60;
+		
+	graphics_draw_circle(ctx, center, radius);
+	
+	time_t now = time(NULL);
+	struct tm *current_time = localtime(&now);
+
+	
+	//hour hand
+	GPoint hourHand;
+	const int16_t hourHandLength = radius*0.6;
+	
+	int32_t hour_angle = (TRIG_MAX_ANGLE * (((current_time->tm_hour % 12) * 6) + (current_time->tm_min / 10))) / (12 * 6);
+	hourHand.x = (int16_t)(sin_lookup(hour_angle) * (int32_t)hourHandLength / TRIG_MAX_RATIO) + center.x;
+	hourHand.y = (int16_t)(-cos_lookup(hour_angle) * (int32_t)hourHandLength / TRIG_MAX_RATIO) + center.y;
+		
+	graphics_context_set_stroke_color(ctx, GColorWhite);
+	graphics_draw_line(ctx, hourHand, center);
+	
+	
+	//minute hand
+	GPoint minuteHand;
+	const int16_t minuteHandLength = radius*0.75;
+	
+	int32_t minute_angle = TRIG_MAX_ANGLE * current_time->tm_min / 60;
+	minuteHand.x = (int16_t)(sin_lookup(minute_angle) * (int32_t)minuteHandLength / TRIG_MAX_RATIO) + center.x;
+	minuteHand.y = (int16_t)(-cos_lookup(minute_angle) * (int32_t)minuteHandLength / TRIG_MAX_RATIO) + center.y;
+	
+	graphics_context_set_stroke_color(ctx, GColorWhite);
+	graphics_draw_line(ctx, minuteHand, center);
+	
+	
+	//second hand
+	GPoint secondHand;
+	const int16_t secondHandLength = radius*0.9;
+	
+	int32_t second_angle = TRIG_MAX_ANGLE * (current_time->tm_sec % base) / base;
+	secondHand.x = (int16_t)(sin_lookup(second_angle) * (int32_t)secondHandLength / TRIG_MAX_RATIO) + center.x;
+	secondHand.y = (int16_t)(-cos_lookup(second_angle) * (int32_t)secondHandLength / TRIG_MAX_RATIO) + center.y;
+
+	graphics_context_set_stroke_color(ctx, GColorWhite);
+	graphics_draw_line(ctx, secondHand, center);
+}
+
+//Is this method actually necessary? What cleanup is done automatically?
+static void analog_window_unload(Window *window) {
+		
+	layer_destroy(analog_layer);
+	window_destroy(analog_window);
 }
 
 static void center_button_handler(ClickRecognizerRef recognizer, void *context) {
 	
-	if (!window_stack_contains_window(analog_window)) {
 		analog_window = window_create();
 		window_set_fullscreen(analog_window, true);
 		window_stack_push(analog_window, true);
 		window_set_background_color(analog_window, GColorBlack);
+		window_set_click_config_provider(analog_window, (ClickConfigProvider) analog_window_config_provider);
+	window_set_window_handlers(analog_window, (WindowHandlers) {
+		.unload = analog_window_unload
+	});
 		
-		Layer *window_layer = window_get_root_layer(my_window);
-		GRect bounds = layer_get_bounds(window_layer);
-		analog_layer = layer_create(bounds);
+		analog_layer = layer_create(GRect(0, 20, 144, 168-20));
 		layer_set_update_proc(analog_layer, analogLayer_update_callback);
 		layer_add_child(window_get_root_layer(analog_window), analog_layer);
-	}
+	
+		layer_add_child(window_get_root_layer(analog_window), text_layer_get_layer(base_layer));
 }
 
-static void window_config_provider(void *context) {
+static void my_window_config_provider(void *context) {
 	window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler)up_button_handler);
 	window_single_click_subscribe(BUTTON_ID_DOWN, (ClickHandler)down_button_handler);
 	window_single_click_subscribe(BUTTON_ID_SELECT, (ClickHandler)center_button_handler);
+}
+
+static void analog_window_config_provider(void *context) {
+	window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler)up_button_handler);
+	window_single_click_subscribe(BUTTON_ID_DOWN, (ClickHandler)down_button_handler);
 }
 
 void handle_init(void) {
@@ -91,7 +149,7 @@ void handle_init(void) {
 	  window_stack_push(my_window, true);
 	  window_set_background_color(my_window, GColorBlack);
 		
-	  window_set_click_config_provider(my_window, (ClickConfigProvider) window_config_provider);
+	  window_set_click_config_provider(my_window, (ClickConfigProvider) my_window_config_provider);
 
 	  time_layer = text_layer_create(GRect(0, 20, 144, 168-20));
 	  text_layer_set_text_color(time_layer, GColorWhite);
@@ -112,7 +170,6 @@ void handle_init(void) {
 	
 	  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(time_layer));
 	  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(base_layer));
-	
 	  
 }
 
@@ -153,5 +210,10 @@ static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
 	//strftime(time_text, sizeof(time_text), "%T", tick_time); //This does the normal time
 	text_layer_set_text(time_layer, time_text);
 	text_layer_set_text(base_layer, base_text);
+	
+	//This is a crude hack. Why does popping the top window kill the base_layer?
+	if (!layer_get_window(text_layer_get_layer(base_layer))) {
+		layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(base_layer));
+	}
 	
 }
